4dc33e9d4de251de43a54bafd2632c94664be0f8
Alec Loudenback <alecloudenback@gmail.com>
2013-01-18 10:52:21 -0500

    Add Clock and HTML page
    To manager internal schedule of passengers

--- sandbox.js
+++ sandbox.js
@@ -5,7 +5,7 @@ function Platform(name) {
     this.push = function(person) {
         this.queue.push(person);
     };
-    this.pop = function {
+    this.pop = function() {
         this.queue.pop();
     };
 }
@@ -14,6 +14,7 @@ function Platform(name) {
 function Passenger(id) {
     this.destination = "";
     this.waiting = false;
+    this.countDown = 0;
     this.
     this.enter = function(place) {
         if (place instanceof Platform) {
@@ -26,9 +27,28 @@ function Passenger(id) {
     }
 }
 
+// Clock
+// An object that govern's a passenger's time at a destination
+function Clock(mu, sigma) {
+    this.mu = mu || 0;
+    this.sigma = sigma || 1;
+    this.rnorm = function() {
+
+        // use a Box-Muller transform to generate a random normal
+        u1 = Math.random();
+        u2 = Math.random();
+
+        stdNorm = Math.sqrt(-2 * Math.log(u1)) * Math.sin(2 * Math.PI * u2);
+        return this.mu + this.sigma * stdNorm;
+    }
+    this.stayLength = function() {
+        return this.rnorm();
+    }
+}
+
+
 // World
 // A place to keep track of all of the objects
-
 function World() {
     this.passengers = [];
     this.stations = [];
